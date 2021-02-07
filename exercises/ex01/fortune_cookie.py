938a071f2f16a63df37206b5b9f3c991b4837120
David Burriss <davhenry@live.unc.edu>
2021-02-07 17:06:43 -0500

    Updated with Answer

--- exercises/ex01/fortune_cookie.py
+++ exercises/ex01/fortune_cookie.py
@@ -1,6 +1,6 @@
 """Program that outputs one of at least four random, good fortunes."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730279779"
 
 # The randint function is imported from the random library so that
 # you are able to generate integers at random.
@@ -13,3 +13,18 @@ from random import randint
 
 
 # Begin your solution here...
+print("Your fotune cookie says...")
+fortune: int = int(randint(1,4))
+
+if fortune == 1:
+    print("You will encounter great fortune.")
+else: 
+    if fortune == 2:
+        print("Your family will prosper.")
+    else: 
+        if fortune == 3:
+            print("A loved one will flourish.")
+        else:
+            if fortune == 4:
+                print("The sun will be bright for you.") 
+print("Now, go spread positive vibes!")
